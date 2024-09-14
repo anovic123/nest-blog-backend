7b9ad00dd45d43c3d9b1c618decbc0dd4d05eece
anovic123 <anovicjosip@gmail.com>
2024-09-14 21:20:45 +0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+const PORT = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => {
+    console.log('App starting listen port: ', PORT);
+  });
+}
+
+bootstrap();
